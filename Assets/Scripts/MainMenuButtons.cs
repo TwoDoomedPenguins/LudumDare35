09120a7b16d3150cabbf1edca252c0822850937a
khuser <koemeterion@googlemail.com>
2016-04-17 23:09:28 +0200

    MainMenu Stuff

---
+++ Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuButtons : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ButtonNewGame()
+    {
+
+        Application.LoadLevel(1);
+
+    } // END ButtonNewGame
+
+    public void ButtonTutorial()
+    { 
+        
+        /* 
+         
+         Hier muss noch ein bisschen Code eingefügt werden!!!!!!
+         
+         */
+
+    } // END ButtonTutorial
+
+    public void ButtonExit()
+    {
+        Application.Quit();
+    } // END ButtonExit
+
+} // END Class
